b045dddb74e089b62e5d1bc003b10b1d69f0548f
ladyengineer1 <wambuidorris3@gmail.com>
2025-06-21 13:04:20 +0300

    Second Commit

--- Day2of13thJune.cpp
+++ Day2of13thJune.cpp
@@ -4,27 +4,66 @@ using namespace std;
 class Tracker
 {
     public:
-      int spendingrate,amount,balance,date,year,month,numitems,price,i ;
-      string comments, 
+     float Totalspending = 0;
+     
 // Users input on her spending
-int Expenses()
+void  Expenses()
 {
-    cout << "How much in the scale of 1 to 10 do you feel your  spending has been today?";
+    int spendingrate;
+    string comment;
+
+    // Users feeling towards her spending
+    cout << "How much in the scale of 1 to 10 do you feel your  spending has been today with 10 as maximum spending?";
     cin >> spendingrate;
-    cout <<"What exactly are the things you bought with their prices ?";
+
+    if(spendingrate>10||spendingrate<0)
+    cout<<"That is an invalid input";
+    else if(spendingrate>=7)
+    cout<<" Guilty !!Today you went all in. Next time prepare a list before making any purchase";
+    else if (spendingrate>=4)
+    cout<<"Good efforts!! You seem to have put good efforts ";
+    else 
+    cout <<"Keep up the good efforts. Your account will appreciate that";
+
+// user to list her purchases
+    cout <<"\n What exactly are the things you bought with their prices ?";
+
+    string items;
+    float price;
+
+     cout <<"What are the things that you bought with their prices? If you have finished your inputs write(done)";
+     
     while (true)
     {
-        cout <<"What are the things that you bought with their prices? If you have finished your inputs write(finish)";
-        cin<< items << price;
-        if (items=="finish")
+       
+        cin>> items ;
+        if (items=="done")
         {break;}
 
+        cin>>price;
+        cout << items<<"\t-\t"<<price;
 
-        cout << items <<"\t-\t"<< price;
+        
+
+        Totalspending+=price;
+        cout<<"\n"<<Totalspending;
         
     }
 }
-    
+
+void  spendinglimit()
+   
+{
+  const int spendlimit = 4500;
+  if (Totalspending>spendlimit)
+  {
+    cout<<"Girl you are out of the budget!!You gonna be broke if you continue with this kind of behaviour. No lies.TAFADHALI BADILISHA!Stick to yhe budget";
+  }
+ else 
+ cout <<"You did nice today.\n You continue this way and You gonna be grateful over time. \n I am so proud of me!";
+}
+
+
 };
 
     
@@ -33,4 +72,5 @@ int Expenses()
 int main(){ 
 Tracker Clothing;
 Clothing.Expenses();
+Clothing.spendinglimit();
 }
